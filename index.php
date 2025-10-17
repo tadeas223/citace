d24f595ad43a94224e1a9e6bea030a628307210e
Tadeáš Máca <maca@SPSEJECNANET.CZ>
2025-10-17 13:18:59 +0200

    pi ejdz pi

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <html>
   <body>
     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdDXn-0lG1ym-zHe1EDJ5LF4pecZO3-wcoug&s"></img>
+    <?php
+      echo "Hello, World!";
+    ?>
+
   </body>
 
 </html>
