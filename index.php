dc9958d693ccb6e5f6be4a5b95e17c26f403da0d
Tadeáš Máca <maca@SPSEJECNANET.CZ>
2025-10-17 13:25:10 +0200

    server

--- index.php
+++ index.php
@@ -1,10 +1,53 @@
-<html>
-  <body>
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdDXn-0lG1ym-zHe1EDJ5LF4pecZO3-wcoug&s"></img>
-    <?php
-      echo "Hello, World!";
-    ?>
+<?php
+require __DIR__ . '/vendor/autoload.php';
 
-  </body>
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
 
-</html>
+class Chat implements MessageComponentInterface {
+    protected $clients;
+
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn) {
+        // Store the new connection
+        $this->clients->attach($conn);
+        echo "New connection: ({$conn->resourceId})\n";
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+        $numRecv = count($this->clients) - 1;
+        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n",
+            $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
+
+        foreach ($this->clients as $client) {
+            // Send message to everyone including sender
+            $client->send($msg);
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+        // The connection is closed, remove it
+        $this->clients->detach($conn);
+        echo "Connection {$conn->resourceId} has disconnected\n";
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        echo "Error: {$e->getMessage()}\n";
+        $conn->close();
+    }
+}
+
+// Run the server
+use Ratchet\Server\IoServer;
+
+$server = IoServer::factory(
+    new Chat(),
+    8080
+);
+
+echo "WebSocket server started on port 8080\n";
+
+$server->run();
